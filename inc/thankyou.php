2981bc4de50dd7921da9de9b62f7c860b885a785
Ritesh3542 <ritiesh3542@gmail.com>
2022-01-12 21:44:17 +0530

    thankyouform

--- inc/thankyou.php
+++ inc/thankyou.php
@@ -26,7 +26,7 @@ if (mysqli_query($conn, "UPDATE `orders` SET  `seen`=1, `status`=0 WHERE `order_
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/png" href="https://img.icons8.com/color/2x/cocktail.png"/>
+	<link rel="icon" type="image/png" href="https://img.icons8.com/color/2x/cocktail.png" />
 	<script type='text/javascript' src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/0.4.2/sweet-alert.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/0.4.2/sweet-alert.css">
 </head>
@@ -34,13 +34,16 @@ if (mysqli_query($conn, "UPDATE `orders` SET  `seen`=1, `status`=0 WHERE `order_
 <body class="bg-dark">
 	<script>
 		sweetAlert({
-				title: "Thank you for Your Contribution ...!",
-				text: "Just wait for the email!<?php echo $pid; ?>",
-				type: "success"
-			},
-			function() {
-				window.location.href = '../feedback.php';
-			});
+			title: 'Thank you for Your Contribution ...!',
+			text: "Lets order something more..!!",
+			showConfirmButton: true,
+			showCancelButton: true,
+			confirmButtonText: `Yes`,
+			cancelButtonText: `No`,
+		},function(){
+			window.location.href='../index.php';
+		}
+		);
 	</script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
